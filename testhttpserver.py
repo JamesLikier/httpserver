f49158c6d1c795e074b629b08946af1644e8c46b
James Likier <james.likier@gmail.com>
2022-07-30 16:23:26 -0500

    Added some easier access to testing functionality in browser

--- testhttpserver.py
+++ testhttpserver.py
@@ -1,18 +1,31 @@
 import httpserver
 import socket
 
-f = open("form.html", "r")
-form = f.read().encode()
-f.close()
-
 s = httpserver.httpserver("localhost",80)
 
 @s.register("GET,POST","/")
 def root(req: httpserver.httprequest, sock: socket.socket):
-    resp = httpserver.httpresponse()
-    resp.body = form
-    resp.statuscode = 200
-    resp.send(sock)
+    with open("main.html","r") as f:
+        resp = httpserver.httpresponse()
+        resp.body = f.read().encode()
+        resp.statuscode = 200
+        resp.send(sock)
+
+@s.register("GET,POST","/multipart?raw")
+def multipartraw(req: httpserver.httprequest, sock: socket.socket):
+    with open("multipartform.html","r") as f:
+        resp = httpserver.httpresponse()
+        resp.body = f.read().encode().replace(b'@placeholder', req.raw)
+        resp.statuscode = 200
+        resp.send(sock)
+
+@s.register("GET,POST","/multipart?body")
+def multipartraw(req: httpserver.httprequest, sock: socket.socket):
+    with open("multipartform.html","r") as f:
+        resp = httpserver.httpresponse()
+        resp.body = f.read().encode().replace(b'@placeholder',str(req.body).encode())
+        resp.statuscode = 200
+        resp.send(sock)
 
 print("Starting server...\n")
 s.start()
