e89ac921977072ea82e9d84f7c2f00ac481ec560
James Likier <james.likier@gmail.com>
2022-07-31 20:26:55 -0500

    Removed some debugging prints

--- testhttpserver.py
+++ testhttpserver.py
@@ -30,13 +30,11 @@ def multipartraw(req: httpserver.httprequest, sock: socket.socket):
 @s.registerstatic("/static/")
 def static(req: httpserver.httprequest, sock: socket.socket):
     resp = httpserver.httpresponse()
-    print(req.uri)
     try:
         with open("."+req.uri, "rb") as f:
             resp.body = f.read()
             resp.statuscode = 200
     except:
-        print(f'{req.uri} NOT FOUND')
         resp.statuscode = 404
     resp.send(sock)
 
