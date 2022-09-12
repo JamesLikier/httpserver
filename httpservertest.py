1b4602550865dbcc9bc1044212ecbdbae663c874
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-09-12 17:27:46 -0500

    Finished skeleton of test cases.

--- httpservertest.py
+++ httpservertest.py
@@ -30,3 +30,31 @@ class TestRequest(unittest.TestCase):
         raise Exception
     def test_fromSocket(self):
         raise Exception
+
+class TestFormData(unittest.TestCase):
+    def test_init(self):
+        raise Exception
+    def test_formatURLEnc(self):
+        raise Exception
+    def test_formatMultiPart(self):
+        raise Exception
+    def test_asStr(self):
+        raise Exception
+    def test_asBytes(self):
+        raise Exception
+    def test_asBool(self):
+        raise Exception
+    def test_asInt(self):
+        raise Exception
+    
+class TestForm(unittest.TestCase):
+    def test_init(self):
+        raise Exception
+    def test_addAndGetField(self):
+        raise Exception
+    def test_fromURLEncStr(self):
+        raise Exception
+    def test_fromMultiPartBytes(self):
+        raise Exception
+    def test_format(self):
+        raise Exception
